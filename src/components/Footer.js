3b9d31ee71fb3ebfe11459dc748b719a781823bb
Jenfi <jennifer_fick@hotmail.com>
2020-03-02 12:59:09 +0100

    added styling to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import '../styling/footer.css'
 
 export const Footer = () => {
   return (
     <footer>
-      <p>Footer</p>
+      <ul>
+        <li>About</li>
+        <li>..</li>
+        <li>..</li>
+      </ul>
+      <ul>
+        <li>Contact</li>
+        <li>..</li>
+        <li>..</li>
+      </ul>
+      <ul>
+        <li>Customer Service</li>
+        <li>..</li>
+        <li>..</li>
+      </ul>
     </footer>
   )
 }
