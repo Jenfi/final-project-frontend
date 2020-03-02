b0e9f4567a47b4c7065ebcbc8eabc89fe17f6911
Clara Gyökeres <you@example.com>
2020-03-02 16:42:20 +0100

    styled the homepage and created button

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import "../styling/homepage.css"
 
-// här vill vi ha en menu bar, en hero-section, lista med ads, modal-box, footer, header
+//DET HÄR ÄR HOMEPAGE
+
+// här vill vi ha en beksrivning av sidan, menu bar, en hero-section, lista med ads, modal-box, footer, header
 
 export const HomePage = () => {
   return (
-    <h2>Home</h2>
+    <section className="hero">
+      <h2 className="hero-title">Furniture made easy.</h2>
+      <p className="hero-tagline">We help you sell your used furniture and give them a new home.</p>
+      <button type="button" className="CTA-button">Start selling</button>
+    </section>
   )
 }
