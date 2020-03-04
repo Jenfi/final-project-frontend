eb12f89dee07147c99d308e0984032cc1fcebce6
Jenfi <jennifer_fick@hotmail.com>
2020-03-04 15:59:54 +0100

    added hero-pictures and slider animation

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,10 @@ import React from 'react'
 import "../styling/homepage.css"
 import yellowchair from "../assets/images/yellowchair.jpeg"
 import "../styling/adcard.css"
+import vase from "../assets/images/pot.jpg"
+import chair from "../assets/images/green-chair.jpg"
+import leatherSofa from "../assets/images/leather-couch.jpg"
+import stool from "../assets/images/the-stool.jpg"
 
 //DET HÄR ÄR HOMEPAGE
 
@@ -11,6 +15,14 @@ export const HomePage = () => {
   return (
     <>
       <section className="hero">
+        <div className="auto-slider">
+          <ul className="auto-slider_content">
+            <li> <img className="hero-image" src={chair} alt="Chair" /></li>
+            <li> <img className="hero-image" src={vase} alt="Vase" /></li>
+            <li> <img className="hero-image" src={stool} alt="Chair and stool" /></li>
+            <li> <img className="hero-image" src={leatherSofa} alt="Sofa" /></li>
+          </ul>
+        </div>
         <h2 className="hero-title">Furniture made easy.</h2>
         <p className="hero-tagline">We help you sell your used furniture and give them a new home.</p>
         <button type="button" className="CTA-button">Start selling</button>
