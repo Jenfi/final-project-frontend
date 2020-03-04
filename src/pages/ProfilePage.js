1d743e86683fb0aa5f4ac58993b3cc938f54d4d2
Clara Gyökeres <you@example.com>
2020-03-04 15:23:36 +0100

    updated the profile page

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import '../styling/profilepage.css'
+import userlogo from '../assets/images/userlogo.png'
 
 // här vill vi visa kontaktinfo samt aktiva annonser (kunna ändra annons?)
+// Hälsa på den inoggades namn 
 // Stretch: om vi vill lägga till favoriter vill vi kunna se dem här 
 
 export const ProfilePage = () => {
   return (
-    <h2>Profile</h2>
+    <main className="userprofile">
+      <img className="userlogo" src={userlogo} alt="User Logo" />
+      <section className="profile-box">
+        <h2>Hi (name)!</h2>
+        <p>Username</p>
+        <p> example@example.com </p>
+      </section>
+    </main>
   )
 }
