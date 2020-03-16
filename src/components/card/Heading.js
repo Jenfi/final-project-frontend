03d966863e4eb4b36e9c90bc3ffeb89936691d3b
Emmie Nilsson <emmie.nilsson@gmail.com>
2020-03-16 20:10:10 +0100

    Changed grid spacing between rows, fixed issue with heading for cards that rendred wrong and sizing of card titles

--- src/components/card/Heading.js
+++ src/components/card/Heading.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export const Heading = ({ level, text, className, ...props }) => {
-  if (level === "h2") return <h2 className={className}>{props.children}</h2>
-  if (level === "h3") return <h3 className={className}>{props.children}</h3>
-  if (level === "h4") return <h4 className={className}>{props.children}</h4>
+  if (level === "2") return <h2 className={className}>{props.children}</h2>
+  if (level === "3") return <h3 className={className}>{props.children}</h3>
+  if (level === "4") return <h4 className={className}>{props.children}</h4>
   return <h1 className={className}>{props.children}</h1>
 }
