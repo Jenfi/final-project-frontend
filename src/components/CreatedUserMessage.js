66726db99e59fab5242631bee48b0121c8dc5bd4
Emmie Nilsson <emmie.nilsson@gmail.com>
2020-03-27 11:48:10 +0100

    Cleaned up css an classnames for created user message.

--- src/components/CreatedUserMessage.js
+++ src/components/CreatedUserMessage.js
@@ -6,10 +6,10 @@ export const CreatedUserMessage = () => {
   return (
     <article className="created-user-container">
       <h1 className="created-user-title">Welcome!</h1>
-      <p className="created-user-infotext">Your user was successfully created.</p>
+      <p className="created-user-infotext">Your account was successfully created.</p>
       <Link
         to="/sign-in"
-        className="to-sign-in">
+        className="created-user-button">
         Sign in
       </Link>
     </article>
